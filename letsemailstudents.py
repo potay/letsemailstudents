9e954cb1e7ba486e0f05e0cfa64adc8266b9d4af
Paul Chun <me@paulchun.com>
2014-10-13 16:21:41 -0400

    Initial commit

---
+++ letsemailstudents.py
@@ -0,0 +1,197 @@
+import getpass
+import smtplib
+import sys, time
+
+def printEmailPreview(testList, CAFullName, CAName,
+                     coCAEmail, fromaddr, passwd,
+                     msgBase, msgVars, subjectBase):
+    andrewid = testList[0][0]
+    firstname = testList[0][2]
+    fullname = testList[0][1]
+    toaddr = "%s@andrew.cmu.edu" % andrewid
+    toaddrList = [toaddr, toaddr]
+    subject  = '[%s] %s' % (andrewid, subjectBase)
+    msgStudentBase = msgSub(msgBase, testList[0], msgVars)
+    if (coCAEmail == None):
+        coCAEmail = ""
+    else:
+        toaddrList += [coCAEmail]
+        coCAEmail = (", %s" % coCAEmail)
+    msg = ("""From: %s <%s>
+To: %s <%s>
+CC: %s <%s>""" + coCAEmail + """
+Subject: %s
+
+Hey %s!
+
+%s
+
+Best,
+%s""") % (CAFullName, fromaddr, fullname, toaddr, CAFullName, fromaddr,
+          subject, firstname, msgStudentBase, CAName)
+
+    # Print sample email
+    print
+    print "EMAIL PREVIEW"
+    print "##################################################################"
+    print
+    print msg
+    print
+    print "##################################################################"
+    print
+
+def sendEmailToList(server, studentList, CAFullName, CAName,
+                    coCAEmail, fromaddr, passwd,
+                    msgBase, msgVars, subjectBase):
+    for studentDetails in studentList:
+        andrewid = studentDetails[0]
+        firstname = studentDetails[2]
+        fullname = studentDetails[1]
+        toaddr = "%s@andrew.cmu.edu" % andrewid
+        toaddrList = [toaddr, toaddr]
+        subject  = '[%s] %s' % (andrewid, subjectBase)
+        msgStudentBase = msgSub(msgBase, studentDetails, msgVars)
+        if (coCAEmail == None):
+            coCAEmail = ""
+        else:
+            toaddrList += [coCAEmail]
+            coCAEmail = (", %s" % coCAEmail)
+        msg = ("""From: %s <%s>
+To: %s <%s>
+CC: %s <%s>""" + coCAEmail + """
+Subject: %s
+
+Hey %s!
+
+%s
+
+Best,
+%s""") % (CAFullName, fromaddr, fullname, toaddr, CAFullName, fromaddr,
+          subject, firstname, msgStudentBase, CAName)
+
+        # The actual mail send
+        try:
+            server.sendmail(fromaddr, toaddrList, msg)
+            print "%s: Sent!" % andrewid
+        except smtplib.Exception:
+            print "%s: Sending failed ):"
+
+def msgSub(msgBase, details, indexList):
+    detailSubs = tuple([details[i] for i in indexList])
+    return msgBase % detailSubs
+
+def loginGmail():
+    print "Gmail Login"
+    print "###################################"
+    CAAndrewID = raw_input("What's your andrew id?: ")
+    fromaddr = "%s@andrew.cmu.edu" % CAAndrewID
+    passwd = getpass.getpass("CMU Google Apps Password? (Not Andrew Password): ")
+
+    username = fromaddr
+    password = passwd
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+
+    try:
+        server.login(username,password)
+        print "** Login success! **"
+        print
+        return (server, CAAndrewID, username, password)
+    except smtplib.SMTPAuthenticationError:
+        answer = raw_input("Oops! Login failed ): Try again? (Y/N): ")
+        if (answer == "Y"):
+            server.quit()
+            loginGmail()
+        else:
+            return (None, None, None, None)
+
+def main():
+    msgSent = False
+    msgfilename = raw_input("What is the filename of your message file (w/o extension .py)?: ")
+
+    try:
+        print
+        print "Loading message file...",
+        _temp = __import__("messages.%s" % msgfilename, globals(), locals(),
+                       ['emaiList', 'msgBase', 'msgVars', 'subjectBase'],
+                       -1)
+        emailList = _temp.emailList
+        msgBase = _temp.msgBase
+        msgVars = _temp.msgVars
+        subjectBase = _temp.subjectBase
+
+        print "Loaded!"
+        print
+
+    except:
+        print "\nMessage file could not be loaded. Either no such message file was found or there is an error with your file."
+        print sys.exc_info()[0]
+
+        return False
+
+    print "Your Details"
+    print "###################################"
+    CAFullName = raw_input("What is your full name?: ")
+    CAName = raw_input("What is your preferred name?: ")
+    print
+
+    (server, CAAndrewID, fromaddr, passwd) = loginGmail()
+
+    if (server == None):
+        print "Unable to login to Gmail. Message not sent."
+        return False
+
+    # List of test contacts (andrew ID, fullname, firstname, ...)
+    testList = [tuple([CAAndrewID, CAFullName, CAName] + \
+                      ["placeholder"]*(len(emailList[0]) - 3)),
+                tuple([CAAndrewID, CAFullName+"2", CAName+"2"] + \
+                      ["placeholder"]*(len(emailList[0]) - 3))]
+
+    print "Co-CA Details"
+    print "###################################"
+    sendToCoCA = raw_input("What's your co-CA's andrew id? Enter 'N' if you do not wish to CC your co-CA: ")
+    if sendToCoCA == "N":
+        coCAEmail = None
+        print "Not sending to your Co-CA."
+    else:
+        coCAName = raw_input("What's your co-CA's name?: ")
+        coCAEmail = ("%s@andrew.cmu.edu"%sendToCoCA)
+        print "CC-ing email to %s (%s)" % (coCAName, coCAEmail)
+        print
+
+    sys.stdout.write("Cooking up your email for you")
+    time.sleep(1)
+    sys.stdout.write(".")
+    time.sleep(1)
+    sys.stdout.write(".")
+    time.sleep(1)
+    sys.stdout.write(".\n")
+
+    printEmailPreview(testList, CAFullName, CAName, coCAEmail,
+                      fromaddr, passwd, msgBase, msgVars, subjectBase)
+
+    continueSendingTest = raw_input("Is the sample email alright? Shall we test it by sending it to the test list? (Y/N): ")
+    print
+
+    if (continueSendingTest == "Y"):
+        sendEmailToList(server, testList, CAFullName, CAName, coCAEmail,
+                    fromaddr, passwd, msgBase, msgVars, subjectBase)
+
+        continueSending = raw_input("Is the email alright? Shall we send it to \
+the actual list? (Y/N): ")
+        print
+
+        if (continueSending == "Y"):
+            sendEmailToList(server, emailList, CAFullName, CAName, coCAEmail,
+                             fromaddr, passwd, msgBase, msgVars, subjectBase)
+            msgSent = True
+
+    server.quit()
+
+    if (msgSent == False):
+        print "Message not sent!"
+
+if __name__ == "__main__":
+    main()
+
---
+++ messages/msgtemplate.py
@@ -0,0 +1,34 @@
+#########################################
+#########################################
+###        START EDITING HERE         ###
+#########################################
+#########################################
+
+# Add any extra details you need after the mandatory fields
+
+# List of students (andrew ID, fullname, firstname, ...)
+emailList = [("jdoe", "John Doe", "John"),
+             ("hpeters", "Harry Peters", "Harry")]
+
+# Subject Settings
+subjectBase = "Lorem Ipsum"
+
+# Message Template (with string formatting)
+msgBase = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis a libero vel viverra. Praesent eget nisi arcu. Proin sagittis finibus elit, non consectetur sem molestie nec. Suspendisse sed iaculis sem, nec ullamcorper diam. Sed viverra justo eget ligula faucibus mattis. Vestibulum rhoncus id magna dictum elementum. Cras hendrerit, risus sit amet faucibus imperdiet, eros massa dignissim sapien, in feugiat dui tellus eu odio. Integer maximus egestas nibh nec semper. Nullam pharetra magna quis lectus eleifend, et blandit tellus tincidunt. Vestibulum accumsan pulvinar egestas. Maecenas efficitur cursus purus. Aenean eget ultrices ipsum. Pellentesque sed mattis dui. Praesent sed interdum augue. Nam ut quam a elit consectetur lobortis. Pellentesque quis nisi non lorem vestibulum ornare id in turpis.
+
+Mauris sit amet nisl ut sapien molestie consequat. Sed quis ipsum facilisis, egestas nunc vel, iaculis felis. Donec condimentum dictum egestas. Fusce ut consectetur enim. Nullam vel leo blandit, sollicitudin eros ut, eleifend ligula. Morbi consectetur pellentesque arcu, ac accumsan felis eleifend id. Aliquam erat volutpat. Nam in lectus et purus fringilla vestibulum. Suspendisse aliquam interdum iaculis.
+
+Vestibulum interdum elit nec enim pretium, ac laoreet dui pulvinar. Pellentesque hendrerit, libero vitae ultricies porttitor, nisl felis varius quam, at tincidunt justo est in metus. Ut ante neque, venenatis a metus id, condimentum pretium velit. Aenean eleifend ante neque, nec placerat lorem posuere quis. Sed urna orci, commodo ac tortor vel, rutrum efficitur felis. Integer vel elit sit amet magna convallis aliquet. Nam placerat dui quis dolor volutpat mattis nec at tortor. Sed bibendum at lorem ac hendrerit.
+
+Phasellus ipsum lorem, eleifend et varius egestas, facilisis ut nisl. Nulla facilisi. Praesent ultrices mi vitae eros congue, sed ultricies massa hendrerit. Etiam pellentesque enim eget urna blandit, ac tristique erat placerat. Ut laoreet sem eget elementum tincidunt. Praesent convallis sodales vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla lacus tellus, pharetra ut bibendum sodales, rutrum vel urna. Vivamus pellentesque scelerisque ullamcorper. Ut nec turpis placerat, faucibus dui vel, placerat mauris. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed nulla quam, fermentum ut urna ac, maximus gravida massa. Suspendisse ultricies odio at erat ullamcorper maximus. Donec tempor iaculis laoreet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse congue turpis ut rutrum rhoncus.
+
+Phasellus euismod lacinia tortor eu tempus. Donec massa enim, mattis non risus a, lobortis placerat mi. Ut id viverra enim, sit amet consequat velit. Aenean maximus eget mauris sed venenatis. Duis ante enim, luctus vitae dui quis, ornare vehicula dolor. Mauris risus urna, eleifend eu justo eu, aliquam iaculis sem. Praesent et est vitae est dapibus euismod. Fusce tempor nisi a turpis ultricies, in sodales sem dapibus."""
+
+# List of the index of the detail tuple in order of subbing
+msgVars = []
+
+#########################################
+#########################################
+###         STOP EDITING HERE         ###
+#########################################
+#########################################
