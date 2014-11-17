0423efe335258698eeb7768273ae7445266254c6
Paul Chun <me@paulchun.com>
2014-11-17 16:46:26 -0500

    Remove the erroneous 'g' after time.sleep

--- letsemailstudents.py
+++ letsemailstudents.py
@@ -186,7 +186,7 @@ def main():
     time.sleep(1)
     sys.stdout.write(".\n")
     sys.stdout.flush()
-    time.sleep(1)g
+    time.sleep(1)
 
     printEmailPreview(testList, CAFullName, CAName, coCAEmail,
                       fromaddr, passwd, msgBase, msgVars, subjectBase)
